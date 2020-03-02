b5aa14ca99046d551f7bd697221ff0ae3a02e91b
sapa <satyajitmails@gmail.com>
2020-03-02 20:48:03 +0530

    added social site links

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Satyajit Panda', // Required
+  description: 'Technologist',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://focused-liskov-0e8165.netlify.com/',
+    github: 'https://github.com/satyajitmails',
+    twitter: 'https://twitter.com/p_satyajit',
+    linkedin: 'https://www.linkedin.com/in/satyajitpanda/',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://focused-liskov-0e8165.netlify.com/',
 };
